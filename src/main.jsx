5d6217a937e45be00c75e678921fce33d5759379
Muhammad Minhajul Alam <minhajulalam.muhammad@gmail.com>
2026-03-23 16:06:24 +0600

    feat: add Suspense fallback loader for initial app load

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/sonner";
 import { HelmetProvider } from "react-helmet-async";
 import ScrollToTop from "./components/shared/ScrollToTop";
+import PageLoader from "./components/shared/PageLoader";
 import "./index.css";
 import App from "./App.jsx";
 
@@ -13,9 +14,11 @@ createRoot(document.getElementById("root")).render(
     <HelmetProvider>
       <BrowserRouter>
         <TooltipProvider>
-          <ScrollToTop />
-          <App />
-          <Toaster position="bottom-center" richColors />
+          <Suspense fallback={<PageLoader message="Loading…" />}>
+            <ScrollToTop />
+            <App />
+            <Toaster position="bottom-center" richColors />
+          </Suspense>
         </TooltipProvider>
       </BrowserRouter>
     </HelmetProvider>
