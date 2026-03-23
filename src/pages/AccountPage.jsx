5ac789b44b91804e90a9668eb3307d5b8894dff9
Muhammad Minhajul Alam <minhajulalam.muhammad@gmail.com>
2026-03-23 17:53:57 +0600

    fix: fix account page errors

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import Layout from "../layouts/Layout";
